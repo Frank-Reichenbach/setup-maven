f2bf606a040d70fc8b12f5b71b0b0ef1e62d25e4
Frank Reichenbach <freichenbach@thinkport.digital>
2024-01-29 19:48:19 +0100

    remove obsolete try-catch block in installer.ts

--- src/installer.ts
+++ src/installer.ts
@@ -35,13 +35,8 @@ async function downloadMaven(version: string): Promise<string> {
   const toolDirectoryName = `apache-maven-${version}`;
   const downloadUrl = `https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${version}/apache-maven-${version}-bin.tar.gz`;
   console.log(`downloading ${downloadUrl}`);
-
-  try {
-    const downloadPath = await tc.downloadTool(downloadUrl);
-    const extractedPath = await tc.extractTar(downloadPath);
-    let toolRoot = path.join(extractedPath, toolDirectoryName);
-    return await tc.cacheDir(toolRoot, 'maven', version);
-  } catch (err) {
-    throw err;
-  }
+  const downloadPath = await tc.downloadTool(downloadUrl);
+  const extractedPath = await tc.extractTar(downloadPath);
+  let toolRoot = path.join(extractedPath, toolDirectoryName);
+  return await tc.cacheDir(toolRoot, 'maven', version);
 }
